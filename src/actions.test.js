bf52c287efd8fe252ed7bbab18de2e158d0e51f9
Jinesh Patel <jinesh701@gmail.com>
2018-02-06 16:23:12 -0500

    added tests for restart game and make guess

--- src/actions.test.js
+++ src/actions.test.js
@@ -13,3 +13,21 @@ describe("generateAuralUpdate", () => {
         expect(action.type).toEqual(GENERATE_AURAL_UPDATE)
     })
 })
+
+describe("restartGame", () => {
+    it("Should return an action", () => {
+        const correctAnswer = 40;
+        const action = restartGame(correctAnswer);
+        expect(action.type).toEqual(RESTART_GAME)
+        expect(action.correctAnswer).toEqual(correctAnswer)
+    })
+})
+
+describe("makeGuess", () => {
+    it("Should return an action", () => {
+        const guess = 20;
+        const action = makeGuess(guess)
+        expect(action.type).toEqual(MAKE_GUESS)
+        expect(action.guess).toEqual(guess)
+    })
+})
