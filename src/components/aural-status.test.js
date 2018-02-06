80970198d8cb5222e8cc5f99a7fdf61147029e8f
Jinesh Patel <jinesh701@gmail.com>
2018-02-06 17:24:25 -0500

    added tests for AuralStatus component

---
+++ src/components/aural-status.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import {AuralStatus} from "./aural-status";
+
+describe("<AuralStatus />", () => {
+  it("Renders without crashing", () => {
+    shallow(<AuralStatus />);
+  });
+
+  it("Renders an Aural Status", () => {
+    let test = "Test status";
+
+    let wrapper = shallow(<AuralStatus auralStatus={test} />);
+    expect(wrapper.contains(test)).toEqual(true);
+  });
+});
