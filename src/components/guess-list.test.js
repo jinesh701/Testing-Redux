d7d0c27fc89757a950e0d89387fa7634ca6aa2fa
Jinesh Patel <jinesh701@gmail.com>
2018-02-06 17:25:48 -0500

    added tests for GuessList component

---
+++ src/components/guess-list.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import {GuessList} from "./guess-list";
+
+describe("<GuessList />", () => {
+  it("Renders without crashing", () => {
+    shallow(<GuessList guesses={[]} />);
+  });
+
+  it("Generate a list of guesses", () => {
+    const values = [10, 14, 41];
+    const wrapper = shallow(<GuessList guesses={values} />);
+    const guessItem = wrapper.find("li");
+
+    expect(guessItem.length).toEqual(values.length);
+  });
+});
