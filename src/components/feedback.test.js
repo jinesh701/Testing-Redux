eb78350c73227913db75f1c14b5e7ac094a346bf
Jinesh Patel <jinesh701@gmail.com>
2018-02-06 17:24:39 -0500

    added test for Feedback component

---
+++ src/components/feedback.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import {Feedback} from "./feedback";
+
+describe("<Feedback />", () => {
+  it("Renders without crashing", () => {
+    shallow(<Feedback />);
+  });
+
+  it("Generates feedback", () => {
+    let test = "Test feedback";
+
+    let wrapper = shallow(<Feedback feedback={test} />);
+
+    expect(wrapper.contains(test)).toEqual(true);
+  });
+});
